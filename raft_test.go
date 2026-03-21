8bd75f8d98fe28a0efb5ac99fdcd84ec77a74d66
akuzin <akuzin@MBP-akuzin.local>
2026-03-21 23:28:16 +0500

    test refactor

--- raft_test.go
+++ raft_test.go
@@ -47,6 +47,48 @@ func (c *cluster) getNodes(state State) []node {
 	return ns
 }
 
+func (c *cluster) assertHealthy(t *testing.T) {
+	leader := c.leader(t)
+	leaderCommit := leader.n.StateMachine.CommitIndex()
+	leaderLogs := leader.storage.Commands()
+	asserts.Len(t, leaderCommit+1, leaderLogs)
+
+	followers := c.getNodes(Follower)
+	asserts.Len(t, 2, followers)
+	for _, node := range followers {
+		c.assertFollower(t, node, leader)
+	}
+}
+
+func (c *cluster) assertFollower(t *testing.T, node, leader node) {
+	leaderTerm := leader.n.CurrentTerm()
+	asserts.Equal(t, leaderTerm, node.n.CurrentTerm())
+
+	leaderCommit := leader.n.StateMachine.CommitIndex()
+	asserts.Equal(t, leaderCommit, node.n.StateMachine.CommitIndex())
+
+	logs := node.storage.Commands()
+	leaderLogs := leader.storage.Commands()
+	asserts.Len(t, leaderCommit+1, logs)
+	for i := range logs {
+		asserts.Slice(t, leaderLogs[i], logs[i])
+	}
+}
+
+func (c *cluster) command(t *testing.T, cmd []byte, node node) {
+	logsBefore := node.storage.Commands()
+	node.n.ClientCommand(t.Context(), cmd)
+	logs := node.storage.Commands()
+	asserts.Equal(t, len(logsBefore)+1, len(logs))
+	asserts.Slice(t, cmd, logs[len(logs)-1])
+}
+
+func (c *cluster) leader(t *testing.T) node {
+	leaders := c.getNodes(Leader)
+	asserts.Len(t, 1, leaders)
+	return leaders[0]
+}
+
 func (c *cluster) run() {
 	for _, node := range c.nodes {
 		go node.n.Run()
@@ -98,56 +140,31 @@ func TestRaft(t *testing.T) {
 		defer cluster.stop(t.Context())
 		cluster.run()
 		cluster.wait()
-		leaders := cluster.getNodes(Leader)
-		followers := cluster.getNodes(Follower)
-		asserts.Len(t, 1, leaders)
-		asserts.Len(t, 2, followers)
+		cluster.assertHealthy(t)
+		initialLeader := cluster.leader(t)
 
-		initialLeader := leaders[0]
-		initialTerm := initialLeader.n.CurrentTerm()
-		asserts.Equal(t, initialTerm, followers[0].n.CurrentTerm())
-		asserts.Equal(t, initialTerm, followers[1].n.CurrentTerm())
-
-		firstCommand := []byte{1}
-		initialLeader.n.ClientCommand(t.Context(), firstCommand)
-		commitIndex := 0
-		asserts.Equal(t, commitIndex, initialLeader.n.StateMachine.CommitIndex())
-		cmds := initialLeader.storage.Commands()
-		asserts.Len(t, 1, cmds)
-		asserts.Slice(t, firstCommand, cmds[0])
+		cluster.command(t, []byte{1}, initialLeader)
 
 		cluster.wait()
-		for i := 0; i < 2; i++ {
-			asserts.Equal(t, commitIndex, initialLeader.n.StateMachine.CommitIndex())
-			cmds := initialLeader.storage.Commands()
-			asserts.Len(t, 1, cmds)
-			asserts.Slice(t, firstCommand, cmds[0])
-		}
+		cluster.assertHealthy(t)
 
+		initialTerm := initialLeader.n.CurrentTerm()
 		initialLeader.n.Shutdown(t.Context())
 		cluster.wait()
 
-		leaders = cluster.getNodes(Leader)
-		followers = cluster.getNodes(Follower)
+		leaders := cluster.getNodes(Leader)
+		followers := cluster.getNodes(Follower)
 		asserts.Len(t, 1, leaders)
 		asserts.Len(t, 1, followers)
 
 		newTerm := leaders[0].n.CurrentTerm()
 		asserts.Gt(t, initialTerm, newTerm)
-		asserts.Equal(t, newTerm, followers[0].n.CurrentTerm())
+		cluster.assertFollower(t, followers[0], leaders[0])
 
-		secondCommand := []byte{2}
-		leaders[0].n.ClientCommand(t.Context(), secondCommand)
-		secondCommitIndex := 1
-		asserts.Equal(t, secondCommitIndex, leaders[0].n.StateMachine.CommitIndex())
-		cmds = leaders[0].storage.Commands()
-		asserts.Len(t, 2, cmds)
-		asserts.Slice(t, secondCommand, cmds[1])
+		cluster.command(t, []byte{2}, leaders[0])
+
 		cluster.wait()
-		asserts.Equal(t, secondCommitIndex, followers[0].n.StateMachine.CommitIndex())
-		cmds = followers[0].storage.Commands()
-		asserts.Len(t, 2, cmds)
-		asserts.Slice(t, secondCommand, cmds[1])
+		cluster.assertFollower(t, followers[0], leaders[0])
 
 		dead := cluster.getNodes(Dead)
 		asserts.Len(t, 1, dead)
@@ -155,11 +172,7 @@ func TestRaft(t *testing.T) {
 
 		go initialLeader.n.Run()
 		cluster.wait()
-		initialLeaderNewTerm := initialLeader.n.CurrentTerm()
-		asserts.Equal(t, newTerm, initialLeaderNewTerm)
-		asserts.Equal(t, secondCommitIndex, initialLeader.n.StateMachine.CommitIndex())
-		cmds = initialLeader.storage.Commands()
-		asserts.Len(t, 2, cmds)
-		asserts.Slice(t, secondCommand, cmds[1])
+
+		cluster.assertHealthy(t)
 	})
 }
