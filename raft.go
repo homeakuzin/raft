0075f28822cab317561b8707aae842bf9630e1c4
akuzin <akuzin@MBP-akuzin.local>
2026-08-08 17:40:11 +0500

    leader election

---
+++ raft.go
@@ -0,0 +1,464 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"math/rand"
+	"net"
+	"net/http"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type NodeId int
+
+const (
+	None NodeId = iota
+	Node1
+	Node2
+	Node3
+)
+
+type State string
+
+const (
+	Follower  State = "follower"
+	Candidate State = "candidate"
+	Leader    State = "leader"
+)
+
+type RequestVoteArgs struct {
+	Term         int
+	CandidateId  NodeId
+	LastLogIndex int
+	LastLogTerm  int
+}
+
+type RequestVoteReply struct {
+	Peer        NodeId
+	Term        int
+	VoteGranted bool
+}
+
+type AppendEntriesArgs struct {
+	Term     int
+	LeaderId NodeId
+}
+
+type AppendEntriesReply struct {
+	Peer    NodeId
+	Term    int
+	Success bool
+}
+
+type requestVoteRpc struct {
+	args    RequestVoteArgs
+	replyCh chan<- RequestVoteReply
+}
+
+type appendEntriesRpc struct {
+	args    AppendEntriesArgs
+	replyCh chan<- AppendEntriesReply
+}
+
+type Transport interface {
+	Serve(ctx context.Context, requestVoteCallback func(args RequestVoteArgs, replyCh chan<- RequestVoteReply), appendEntriesCallback func(args AppendEntriesArgs, replyCh chan<- AppendEntriesReply)) error
+	Shutdown(ctx context.Context)
+	RequestVote(ctx context.Context, id NodeId, data RequestVoteArgs) (RequestVoteReply, error)
+	AppendEntries(ctx context.Context, id NodeId, data AppendEntriesArgs) (AppendEntriesReply, error)
+}
+
+type Node struct {
+	mu          *sync.Mutex
+	id          NodeId
+	votedFor    NodeId
+	currentTerm int
+	state       State
+	transport   Transport
+	logger      *RaftLogger
+	peers       []NodeId
+	shutdownCh  chan struct{}
+	runWg       sync.WaitGroup
+	rpcWg       sync.WaitGroup
+
+	currentElectionVotes map[NodeId]bool
+	requestVoteRpcCh     chan requestVoteRpc
+	appendEntriesRpcCh   chan appendEntriesRpc
+	requestVoteReplyCh   chan RequestVoteReply
+	appendEntriesReplyCh chan AppendEntriesReply
+}
+
+func NewNode(id NodeId, peers []NodeId, logger *RaftLogger, transport Transport) *Node {
+	return &Node{mu: &sync.Mutex{}, id: id, logger: logger, peers: peers, transport: transport, shutdownCh: make(chan struct{})}
+}
+
+func (n *Node) Id() NodeId {
+	return n.id
+}
+
+func (n *Node) State() State {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+	return n.state
+}
+
+func (n *Node) CurrentTerm() int {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+	return n.currentTerm
+}
+
+func (n *Node) Shutdown(ctx context.Context) {
+	close(n.shutdownCh)
+	n.transport.Shutdown(ctx)
+	n.rpcWg.Wait()
+	n.runWg.Wait()
+}
+
+func (n *Node) Run(ctx context.Context) error {
+	n.runWg.Add(1)
+	defer n.runWg.Done()
+
+	n.state = Follower
+	n.logger.InfoContext(ctx, "start node")
+	n.requestVoteRpcCh = make(chan requestVoteRpc, len(n.peers))
+	n.requestVoteReplyCh = make(chan RequestVoteReply, len(n.peers))
+	n.appendEntriesRpcCh = make(chan appendEntriesRpc, len(n.peers))
+	n.appendEntriesReplyCh = make(chan AppendEntriesReply, len(n.peers))
+	n.currentElectionVotes = make(map[NodeId]bool, len(n.peers))
+	n.votedFor = -1
+	if err := n.transport.Serve(ctx, func(args RequestVoteArgs, replyCh chan<- RequestVoteReply) {
+		n.logger.dlog2("received RequestVote", "args", args)
+		n.requestVoteRpcCh <- requestVoteRpc{args, replyCh}
+	}, func(args AppendEntriesArgs, replyCh chan<- AppendEntriesReply) {
+		n.logger.dlog2("received AppendEntries", "args", args)
+		n.appendEntriesRpcCh <- appendEntriesRpc{args, replyCh}
+	}); err != nil {
+		n.logger.ErrorContext(ctx, "could not serve raft transport", "error", err)
+		return fmt.Errorf("could not serve raft transport: %w", err)
+	}
+	defer n.transport.Shutdown(ctx)
+	for {
+		if n.eventLoop(ctx) {
+			return nil
+		}
+	}
+}
+
+func (n *Node) eventLoop(ctx context.Context) (stop bool) {
+	n.mu.Lock()
+	defer n.mu.Unlock()
+
+	delta := time.Duration(rand.Int63n(int64(150*time.Millisecond) * 2))
+	electionTimeout := 150*time.Millisecond + delta
+	election := time.NewTimer(electionTimeout)
+	defer election.Stop()
+
+	heartbeat := time.NewTimer(50 * time.Millisecond)
+	defer heartbeat.Stop()
+	if n.state != Leader {
+		heartbeat.Stop()
+	}
+
+	select {
+	case <-ctx.Done():
+		stop = true
+	case <-n.shutdownCh:
+		stop = true
+
+	case <-heartbeat.C:
+		n.logger.dlog("heartbeat timeout")
+		n.sendHeartbeats(ctx, n.appendEntriesReplyCh)
+	case reply := <-n.appendEntriesReplyCh:
+		n.logger.dlog2("handle AppendEntries reply", "peer", reply.Peer, "reply", reply)
+		if reply.Term > n.currentTerm {
+			n.becomeFollower(reply.Term)
+		}
+	case appendEntries := <-n.appendEntriesRpcCh:
+		n.logger.dlog2("handle AppendEntries call", "peer", appendEntries.args.LeaderId, "args", appendEntries.args)
+		var reply AppendEntriesReply
+		reply.Peer = n.id
+		reply.Term = n.currentTerm
+		reply.Success = false
+		if appendEntries.args.Term >= n.currentTerm {
+			reply.Success = true
+			n.becomeFollower(appendEntries.args.Term)
+		}
+		appendEntries.replyCh <- reply
+
+	case <-election.C:
+		n.logger.dlog("start election", "new_term", n.currentTerm+1, "timeout", electionTimeout)
+		n.startElection(ctx, n.requestVoteReplyCh)
+	case reply := <-n.requestVoteReplyCh:
+		n.logger.dlog2("handle RequestVote reply", "peer", reply.Peer, "reply", reply)
+		if reply.Term > n.currentTerm {
+			n.becomeFollower(reply.Term)
+			return
+		}
+		if reply.VoteGranted && n.state == Candidate {
+			n.currentElectionVotes[reply.Peer] = true
+			votes := 1
+			for _, voteGranted := range n.currentElectionVotes {
+				if voteGranted {
+					votes++
+				}
+			}
+			if votes*2 > len(n.peers)+1 {
+				n.logger.dlog("become leader")
+				n.state = Leader
+				n.sendHeartbeats(ctx, n.appendEntriesReplyCh)
+			}
+		}
+	case requestVote := <-n.requestVoteRpcCh:
+		n.logger.dlog2("handle RequestVote call", "peer", requestVote.args.CandidateId, "args", requestVote.args)
+		var reply RequestVoteReply
+		reply.VoteGranted = false
+		if requestVote.args.Term > n.currentTerm {
+			n.becomeFollower(requestVote.args.Term)
+			if n.votedFor == -1 {
+				n.votedFor = requestVote.args.CandidateId
+				reply.VoteGranted = true
+			}
+		}
+		reply.Peer = n.id
+		reply.Term = n.currentTerm
+		requestVote.replyCh <- reply
+	}
+	return
+}
+
+func (n *Node) becomeFollower(term int) {
+	n.logger.dlog("become follower", "term", term)
+	n.state = Follower
+	n.currentTerm = term
+}
+
+func (n *Node) sendHeartbeats(ctx context.Context, replyCh chan<- AppendEntriesReply) {
+	n.logger.dlog("send heartbeats")
+	args := AppendEntriesArgs{
+		Term:     n.currentTerm,
+		LeaderId: n.id,
+	}
+	for _, peer := range n.peers {
+		n.rpcWg.Add(1)
+		go func() {
+			defer n.rpcWg.Done()
+			n.logger.dlog3("send AppendEntries", "peer", peer, "args", args)
+			reply, err := n.transport.AppendEntries(ctx, peer, args)
+			if err != nil {
+				n.logger.dlog("AppendEntries failed", "peer", peer, "error", err)
+				return
+			}
+			reply.Peer = peer
+			replyCh <- reply
+		}()
+	}
+}
+
+func (n *Node) startElection(ctx context.Context, replyCh chan<- RequestVoteReply) {
+	n.state = Candidate
+	n.currentTerm++
+	n.votedFor = n.id
+	args := RequestVoteArgs{
+		Term:         n.currentTerm,
+		CandidateId:  n.id,
+		LastLogIndex: 0,
+		LastLogTerm:  0,
+	}
+	for _, peer := range n.peers {
+		n.rpcWg.Add(1)
+		go func() {
+			defer n.rpcWg.Done()
+			n.logger.dlog3("send RequestVote", "peer", peer, "args", args)
+			reply, err := n.transport.RequestVote(ctx, peer, args)
+			if err != nil {
+				n.logger.dlog("RequestVote failed", "peer", peer, "error", err)
+				return
+			}
+			reply.Peer = peer
+			replyCh <- reply
+		}()
+	}
+}
+
+type SlogLogger interface {
+	Debug(msg string, args ...any)
+	InfoContext(ctx context.Context, msg string, args ...any)
+	ErrorContext(ctx context.Context, msg string, args ...any)
+}
+
+type RaftLogger struct {
+	SlogLogger
+	debugLevel atomic.Int32
+}
+
+func NewRaftLogger(logger SlogLogger) *RaftLogger {
+	return &RaftLogger{SlogLogger: logger}
+}
+
+func (l *RaftLogger) DebugLevel(level int) *RaftLogger {
+	l.debugLevel.Store(int32(level))
+	return l
+}
+
+func (l *RaftLogger) dlog(msg string, args ...any) {
+	if l.debugLevel.Load() > 0 {
+		l.Debug(msg, args...)
+	}
+}
+
+func (l *RaftLogger) dlog2(msg string, args ...any) {
+	if l.debugLevel.Load() > 1 {
+		l.Debug(msg, args...)
+	}
+}
+
+func (l *RaftLogger) dlog3(msg string, args ...any) {
+	if l.debugLevel.Load() > 2 {
+		l.Debug(msg, args...)
+	}
+}
+
+type MemoryTransport struct {
+	node  *Node
+	peers map[NodeId]MemoryTransport
+}
+
+func (t MemoryTransport) Serve(ctx context.Context, requestVoteCallback func(args RequestVoteArgs, replyCh chan<- RequestVoteReply) RequestVoteReply, appendEntriesCallback func(AppendEntriesArgs) AppendEntriesReply) {
+
+}
+
+// func (t MemoryTransport) RequestVote(ctx context.Context, peer NodeId, data RequestVoteArgs) (RequestVoteReply, error) {
+
+// }
+
+// func (t MemoryTransport) AppendEntries(ctx context.Context, peer NodeId, data AppendEntriesArgs) (AppendEntriesReply, error) {
+// }
+
+type HttpPeerTransport struct {
+	ln     net.Listener
+	nodeId NodeId
+	peers  map[NodeId]string
+	server *http.Server
+	logger *RaftLogger
+}
+
+func NewHttpTransport(ln net.Listener, nodeId NodeId, peers map[NodeId]string, logger *RaftLogger) *HttpPeerTransport {
+	return &HttpPeerTransport{ln: ln, nodeId: nodeId, peers: peers, logger: logger}
+}
+
+func (t *HttpPeerTransport) Serve(ctx context.Context, requestVoteCallback func(args RequestVoteArgs, replyCh chan<- RequestVoteReply), appendEntriesCallback func(args AppendEntriesArgs, replyCh chan<- AppendEntriesReply)) error {
+	t.server = &http.Server{Addr: t.ln.Addr().String(), Handler: httpHandler{requestVoteCallback, appendEntriesCallback, t.logger}}
+	go t.server.Serve(t.ln)
+	return nil
+}
+
+func (t *HttpPeerTransport) Shutdown(ctx context.Context) {
+	if t.server != nil {
+		t.server.Close()
+	}
+}
+
+type httpHandler struct {
+	requestVoteCallback   func(args RequestVoteArgs, replyCh chan<- RequestVoteReply)
+	appendEntriesCallback func(args AppendEntriesArgs, replyCh chan<- AppendEntriesReply)
+	logger                *RaftLogger
+}
+
+func (h httpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(404)
+		return
+	}
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(500)
+		h.logger.ErrorContext(r.Context(), "could not read request body", "error", err)
+	}
+
+	if r.RequestURI == "/request-vote" {
+		var args RequestVoteArgs
+		if err := json.Unmarshal(body, &args); err != nil {
+			w.WriteHeader(400)
+		}
+		replyCh := make(chan RequestVoteReply)
+		h.requestVoteCallback(args, replyCh)
+		reply := <-replyCh
+		responseBody, err := json.Marshal(&reply)
+		if err != nil {
+			w.WriteHeader(500)
+			h.logger.ErrorContext(r.Context(), "could not encode request body", "error", err)
+		}
+		w.WriteHeader(200)
+		w.Write(responseBody)
+	} else if r.RequestURI == "/append-entries" {
+		var args AppendEntriesArgs
+		if err := json.Unmarshal(body, &args); err != nil {
+			w.WriteHeader(400)
+		}
+		replyCh := make(chan AppendEntriesReply)
+		h.appendEntriesCallback(args, replyCh)
+		reply := <-replyCh
+		responseBody, err := json.Marshal(&reply)
+		if err != nil {
+			w.WriteHeader(500)
+			h.logger.ErrorContext(r.Context(), "could not encode request body", "error", err)
+		}
+		w.WriteHeader(200)
+		w.Write(responseBody)
+	}
+}
+
+func (t *HttpPeerTransport) RequestVote(ctx context.Context, peer NodeId, data RequestVoteArgs) (RequestVoteReply, error) {
+	payload, err := json.Marshal(data)
+	if err != nil {
+		return RequestVoteReply{}, fmt.Errorf("marshal: %w", err)
+	}
+	req, _ := http.NewRequestWithContext(ctx, "POST", "http://"+t.peers[peer]+"/request-vote", bytes.NewBuffer(payload))
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return RequestVoteReply{}, fmt.Errorf("POST /request-vote: %w", err)
+	}
+	defer resp.Body.Close()
+	resultBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return RequestVoteReply{}, fmt.Errorf("read POST /request-vote response: %w", err)
+	}
+	if resp.StatusCode != 200 {
+		return RequestVoteReply{}, fmt.Errorf("POST /request-vote status code %d: %s", resp.StatusCode, resultBytes[:min(len(resultBytes), 64)])
+	}
+	var result RequestVoteReply
+	if err := json.Unmarshal(resultBytes, &result); err != nil {
+		return RequestVoteReply{}, fmt.Errorf("POST /request-vote returned invalid JSON: %s (%w)", resultBytes[:min(len(resultBytes), 64)], err)
+	}
+	return result, nil
+}
+
+func (t *HttpPeerTransport) AppendEntries(ctx context.Context, peer NodeId, data AppendEntriesArgs) (AppendEntriesReply, error) {
+	payload, err := json.Marshal(data)
+	if err != nil {
+		return AppendEntriesReply{}, fmt.Errorf("marshal: %w", err)
+	}
+	req, _ := http.NewRequestWithContext(ctx, "POST", "http://"+t.peers[peer]+"/append-entries", bytes.NewBuffer(payload))
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return AppendEntriesReply{}, fmt.Errorf("POST /append-entries: %w", err)
+	}
+	defer resp.Body.Close()
+	resultBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return AppendEntriesReply{}, fmt.Errorf("read POST /append-entries response: %w", err)
+	}
+	if resp.StatusCode != 200 {
+		return AppendEntriesReply{}, fmt.Errorf("POST /append-entries status code %d: %s", resp.StatusCode, resultBytes[:min(len(resultBytes), 64)])
+	}
+	var result AppendEntriesReply
+	if err := json.Unmarshal(resultBytes, &result); err != nil {
+		return AppendEntriesReply{}, fmt.Errorf("POST /append-entries returned invalid JSON: %s (%w)", resultBytes[:min(len(resultBytes), 64)], err)
+	}
+	return result, nil
+}
---
+++ raft_test.go
@@ -0,0 +1,133 @@
+package main_test
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log/slog"
+	"net"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	. "github.com/homeakuzin/raft"
+	"github.com/stretchr/testify/assert"
+)
+
+var flagDebugLevel = flag.Int("debug", 0, "node debug level")
+
+var nodeLogColors = map[NodeId]string{
+	Node1: "\x1b[34m",
+	Node2: "\x1b[32m",
+	Node3: "\x1b[35m",
+}
+
+var logColorMu sync.Mutex
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	os.Exit(m.Run())
+}
+
+func TestLeaderIsElected(t *testing.T) {
+	t.Parallel()
+
+	ln1, err := net.Listen("tcp", "0.0.0.0:0")
+	assert.NoError(t, err)
+	ln2, err := net.Listen("tcp", "0.0.0.0:0")
+	assert.NoError(t, err)
+	ln3, err := net.Listen("tcp", "0.0.0.0:0")
+	assert.NoError(t, err)
+
+	addrs := map[NodeId]string{
+		Node1: ln1.Addr().String(),
+		Node2: ln2.Addr().String(),
+		Node3: ln3.Addr().String(),
+	}
+	logger1 := logger(t, Node1)
+	logger2 := logger(t, Node2)
+	logger3 := logger(t, Node3)
+	n1 := NewNode(Node1, []NodeId{Node2, Node3}, logger1, NewHttpTransport(ln1, Node1, addrs, logger1))
+	n2 := NewNode(Node2, []NodeId{Node1, Node3}, logger2, NewHttpTransport(ln2, Node2, addrs, logger2))
+	n3 := NewNode(Node3, []NodeId{Node1, Node2}, logger3, NewHttpTransport(ln3, Node3, addrs, logger3))
+	ctx := t.Context()
+	nodes := []*Node{n1, n2, n3}
+	defer func() {
+		for _, n := range nodes {
+			n.Shutdown(ctx)
+		}
+	}()
+	go n1.Run(ctx)
+	go n2.Run(ctx)
+	go n3.Run(ctx)
+	leader, followers := waitForLeader(t, nodes)
+	for _, n := range followers {
+		assert.Equal(t, leader.CurrentTerm(), n.CurrentTerm())
+	}
+}
+
+func waitForLeader(t testing.TB, nodes []*Node) (leader *Node, followers []*Node) {
+	maxRetries := 50
+	retries := 0
+	pollInterval := time.Millisecond * 15
+	ticker := time.NewTicker(pollInterval)
+	defer ticker.Stop()
+	for range ticker.C {
+		retries++
+		if retries == maxRetries {
+			break
+		}
+		t.Log("poll states")
+		stateMap := map[State][]NodeId{
+			Leader:    make([]NodeId, 0, len(nodes)),
+			Candidate: make([]NodeId, 0, len(nodes)),
+			Follower:  make([]NodeId, 0, len(nodes)),
+		}
+		followers = make([]*Node, 0, len(nodes))
+		leaderCnt := 0
+		for _, n := range nodes {
+			state := n.State()
+			if state == Leader {
+				leaderCnt++
+				leader = n
+			} else if state == Follower {
+				followers = append(followers, n)
+			}
+			stateMap[state] = append(stateMap[state], n.Id())
+		}
+		if leaderCnt == 1 {
+			assert.Len(t, followers, 2, "state map", stateMap)
+			return
+		}
+		assert.LessOrEqual(t, leaderCnt, 1)
+		time.Sleep(pollInterval)
+	}
+	t.Fatal("no leader elected")
+	return
+}
+
+type coloredLogWriter struct {
+	w      io.Writer
+	color  string
+	prefix string
+}
+
+func (w coloredLogWriter) Write(p []byte) (int, error) {
+	logColorMu.Lock()
+	defer logColorMu.Unlock()
+
+	if _, err := fmt.Fprintf(w.w, "%s%s\x1b[0m ", w.color, w.prefix); err != nil {
+		return 0, err
+	}
+	return w.w.Write(p)
+}
+
+func logger(t testing.TB, nodeID NodeId) *RaftLogger {
+	level := slog.LevelDebug
+	return NewRaftLogger(slog.New(slog.NewTextHandler(coloredLogWriter{
+		w:      t.Output(),
+		color:  nodeLogColors[nodeID],
+		prefix: fmt.Sprintf("[Node%d]", nodeID),
+	}, &slog.HandlerOptions{Level: level}))).DebugLevel(*flagDebugLevel)
+}
